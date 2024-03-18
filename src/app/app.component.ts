ccb9031daeb90f05fb3f99f6bb1c05619703950d
jaxybaxy <momen.ahmed_2010@yahoo.com>
2024-03-18 17:25:37 +0200

    first angular thing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SkillsComponent,AboutMeComponent,ProjectsComponent,FooterComponent,HeroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
